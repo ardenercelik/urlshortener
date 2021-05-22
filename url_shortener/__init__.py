db6a29629da02dc420fa1f75c40d5bd8be589a2d
Arden <ardenerc@gmail.com>
2021-05-23 02:26:58 +0300

    db initialize

--- url_shortener/__init__.py
+++ url_shortener/__init__.py
@@ -9,9 +9,9 @@ def create_app(config_file="settings.py"):
     app = Flask(__name__)
 
     app.config.from_pyfile(config_file)
-
-    db.init_app(app)  # init db
-
+    db.init_app(app)
+    with app.app_context():
+        db.create_all()
     app.register_blueprint(short)
 
 
--- url_shortener/models.py
+++ url_shortener/models.py
@@ -7,6 +7,8 @@ from .extensions import db
 
 # Integer, PRi key, URL, visitCount, date   CMD ["pipenv", "run", "flask", "run"]
 class Link(db.Model):
+    __tablename__ = "link"
+    __table_args__ = {'extend_existing': True}
     id = db.Column(db.Integer, primary_key=True)
     original_url = db.Column(db.String(512))
     short_url = db.Column(db.String(5), unique=True)
